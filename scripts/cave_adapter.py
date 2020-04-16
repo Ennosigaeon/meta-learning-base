ec411aeb879c18612a21a2150760fa7046e0ecc5
tim <tim.grams@usu.com>
2020-04-16 16:19:16 +0200

    Added exhaustive feature selection

--- scripts/cave_adapter.py
+++ scripts/cave_adapter.py
@@ -1,8 +1,10 @@
 import base64
 import pickle
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 from automl.components.classification import _classifiers
 import pandas as pd
+import numpy as np
 import os
 
 
@@ -16,7 +18,7 @@ def base_64_to_object(b64str: str):
 
 
 # Specify the algorithm to load
-alg = 'gradient_boosting'
+alg = 'libsvm_svc'
 output_path = '/mnt/c/users/usitgrams/cave_configs/'
 
 # Load data from an algorithm
@@ -53,8 +55,8 @@ for index, algorithm in daten.iterrows():
                'cost': 1 - algorithm['accuracy'],
                'config_id': algorithm['id']}
     trajectory_csv = trajectory_csv.append(pd.DataFrame.from_dict(traject, orient='index').T, ignore_index=True)
-    y = traject['cost']
 trajectory_csv['evaluations'] = trajectory_csv['evaluations'].astype(int)
+y = trajectory_csv['cost']
 
 # Create folder for CAVE configuration files
 if not os.path.exists(output_path + alg):
@@ -88,17 +90,52 @@ labelEncoder = LabelEncoder()
 for colname in categorical:
     X[colname] = labelEncoder.fit_transform(X[colname])
 
-# Exhaustive Feature Selection
+# Impute inactive hyperparameter
+from sklearn.impute import SimpleImputer
+simpleImputer = SimpleImputer(strategy='constant', fill_value=-1)
+X = pd.DataFrame(simpleImputer.fit_transform(X), columns=X.columns)
+
+# Random Forest Feature Importance
 from sklearn.ensemble import RandomForestRegressor
 randomForest = RandomForestRegressor()
 randomForest.fit(X, y)
-from itertools import combinations
-features = X.columns
-for L in range(0, len(features) + 1):
-    for subset in combinations(X.columns, L):
-        print(subset)
+importances = randomForest.feature_importances_
+print('Random Forest Feature Importance Results')
+for i,v in enumerate(importances):
+    print(X.columns.tolist()[i] + ': ' + str(v))
+plt.barh(X.columns.tolist(), importances, align='center', edgecolor='black', color='grey')
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.xlabel('Mittlere Varianzabnahme', fontsize=15)
+plt.show()
 
-# Compute fANOVA externally (plots look better)
+# Exhaustive Feature Selection
+from itertools import combinations
+from sklearn.model_selection import cross_validate
+features = X.columns
+results = []
+best_score = {'mean_score': -100.0}
+for L in range(0, len(features) + 1):
+    for subset in combinations(features, L):
+        cv_scores = cross_validate(RandomForestRegressor(), X[list(subset)], y, cv=5, scoring='neg_root_mean_squared_error')
+        result = {'count': len(subset),
+                        'hyperparameter': list(subset),
+                        'mean_score': np.mean(cv_scores['test_score']),
+                        'cv_scores': cv_scores['test_score']}
+        results.append(result)
+        if result['mean_score'] > best_score['mean_score']:
+            best_score = result
+
+# Plot Exhaustive Feature Selection Importance Values
+results = pd.DataFrame(results)
+mean_scores = pd.DataFrame()
+for k in features:
+    mean_scores = pd.concat([mean_scores, results[results.hyperparameters.apply(lambda x: k in x)]['mean_score']], axis=1)
+mean_scores.columns = features
+mean_scores.boxplot()
+plt.show()
+
+# Compute fANOVA externally (plots look better than from CAVE)
 X = config_csv.drop(columns=['CONFIG_ID'], axis=1).to_numpy()
 y = runhistory_csv['cost'].to_numpy()
 from fanova import fANOVA
