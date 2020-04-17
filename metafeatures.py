Fix calculation of attributes with missing values

metafeatures.py
@@ -199,18 +199,14 @@ class NumberOfFeaturesWithMissingValues(MetaFeature):
 
         if X_object.empty:
             missing = ~np.isfinite(X_numeric)
-            num_missing = missing.sum(axis=0)
-
-            return int(np.sum([1 if num > 0 else 0 for num in num_missing]))
+            return (missing.sum(axis=0) > 0).sum()
         else:
             missing_o = pd.isna(X_object)
-            num_missing_o = missing_o.sum(axis=0)
+            num_missing_o = (missing_o.sum(axis=0) > 0).sum()
 
             missing_n = ~np.isfinite(X_numeric)
-            num_missing_n = missing_n.sum(axis=0)
-            num_missing = num_missing_n + num_missing_o
-
-            return int(np.sum([1 if num > 0 else 0 for num in num_missing]))
+            num_missing_n = (missing_n.sum(axis=0) > 0).sum()
+            return num_missing_n + num_missing_o
 
 
 class ClassOccurrences(MetaFeature):